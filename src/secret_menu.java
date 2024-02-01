db76e066a990f4793b03053a44d87922d9a87498
현성 <womok2@ajou.ac.kr>
2024-02-01 23:25:28 +0900

    new problem solved 2024/02/01

--- src/secret_menu.java
+++ src/secret_menu.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.*;
-
+//
 public class secret_menu {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
